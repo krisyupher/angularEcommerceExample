4bc0b0d7b693632e73e2521f0f4bb13a35a760d0
Cristian Florez <48220112+krisyupher@users.noreply.github.com>
2021-08-05 20:35:11 -0500

    Revert "deplying to heroku"
    This reverts commit 90320ff161886efa33167ea5fc2e2b2964cbd1ec.

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/exampleEcommerce'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/exampleEcommerce/index.html'));});
-app.listen(process.env.PORT || 8080);
